d0537d721eb869526400c9c7024055bf7d0e9ee3
rspt <julien@rspt.ch>
2021-10-27 11:20:59 +0200

    Migrate Forms style to Bootstrap

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,12 +3,19 @@
 @section('content')
     <h1>Login</h1>
 
-    <form class="" action="{{ route('auth.authenticate') }}" method="post">
+    <form action="{{ route('auth.authenticate') }}" method="post">
         @csrf
-        Username: <input type="text" name="username" value="">
-        <br>
-        Password: <input type="password" name="password" value="" required>
-        <br>
-        <button type="submit" name="button">Login</button>
+
+        <div class="mb-3">
+            <label for="username" class="form-label">Username</label>
+            <input type="text" class="form-control" id="username" name="username" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control" id="password" name="password" required>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Login</button>
     </form>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,12 +3,19 @@
 @section('content')
     <h1>Create account</h1>
 
-    <form class="" action="{{ route('auth.createAccount') }}" method="post">
+    <form action="{{ route('auth.createAccount') }}" method="post">
         @csrf
-        Username: <input type="text" name="username" value="" required>
-        <br>
-        Password: <input type="password" name="password" value="" required>
-        <br>
-        <button type="submit" name="button">Create</button>
+
+        <div class="mb-3">
+            <label for="username" class="form-label">Username</label>
+            <input type="text" class="form-control" id="username" name="username" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control" id="password" name="password" required>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Create account</button>
     </form>
 @endsection
