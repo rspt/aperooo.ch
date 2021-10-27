febafc6dc562397327a45173fc64a57f49990706
rspt <julien@rspt.ch>
2021-10-27 11:08:11 +0200

    Bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,29 +4,43 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta charset="utf-8">
         <title>Apérooo</title>
+
+        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     </head>
     <body>
-        <nav>
-            <ul>
-                <li>
-                    <a href="{{ route('home') }}">Home</a>
-                </li>
-                @guest
-                    <li>
-                        <a href="{{ route('auth.register') }}">Create account</a>
-                    </li>
-                    <li>
-                        <a href="{{ route('auth.login') }}">Login</a>
-                    </li>
-                @endguest
-                @auth
-                    <li>
-                        <a href="{{ route('auth.logout') }}">{{ Auth::user()->username }} - Logout</a>
-                    </li>
-                @endauth
-            </ul>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="{{ route('home') }}">Apéroo</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a href="{{ route('home') }}" class="nav-link">Home</a>
+                        </li>
+                        @guest
+                            <li class="nav-item">
+                                <a href="{{ route('auth.register') }}" class="nav-link">Create account</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ route('auth.login') }}" class="nav-link">Login</a>
+                            </li>
+                        @endguest
+                        @auth
+                            <li class="nav-item">
+                                <a href="{{ route('auth.logout') }}" class="nav-link">{{ Auth::user()->username }} - Logout</a>
+                            </li>
+                        @endauth
+                    </ul>
+                </div>
+            </div>
         </nav>
 
-        @yield('content')
+        <div class="container-fluid">
+            @yield('content')
+        </div>
+
+        <script src="{{ mix('/js/app.js') }}"></script>
     </body>
 </html>
